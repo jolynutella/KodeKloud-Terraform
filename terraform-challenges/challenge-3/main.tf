2bc670ee026061ec998b7528efc1a3a8b72108ea
Ihor Bytskalo <ihor_bytskalo@epam.com>
2026-02-04 13:03:29 +0200

    Complete challenge 3

---
+++ terraform-challenges/challenge-3/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "citadel" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  user_data     = file("install-nginx.sh")
+}
+
+resource "aws_key_pair" "citadel-key" {
+  key_name   = "citadel"
+  public_key = file("/root/terraform-challenges/project-citadel/.ssh/ec2-connect-key.pub")
+}
+
+resource "aws_eip" "eip" {
+  instance = aws_instance.citadel.id
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_dns} > /root/citadel_public_dns.txt"
+  }
+}
