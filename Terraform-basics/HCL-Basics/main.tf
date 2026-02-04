ec4f13aa74283a153cf80437edcc549449165606
Ihor Bytskalo <ihor_bytskalo@epam.com>
2026-02-04 11:03:50 +0200

    Delete terraform basics course

--- Terraform-basics/HCL-Basics/main.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "games" {
-    filename = "/Users/ihorb/favorite-game.txt"
-    content = "The Witcher 3"
-}
-
-resource "local_sensitive_file" "dishes" {
-    filename = "/Users/ihorb/favorite-dish.txt"
-    content = "Pizza Pepperoni"
-}
--- Terraform-basics/Resource-Attributes/main.tf
+++
@@ -1,7 +0,0 @@
- resource "time_static" "time_update" {
-}
-
-resource "local_file" "time" {
-  filename = "/Users/ihorb/time.txt"
-  content  = "Time stamp of this file is ${time_static.time_update.id}"
-}
