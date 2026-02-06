d08883923549863e6385e85163d9a7f598f4c924
Ihor Bytskalo <ihor_bytskalo@epam.com>
2026-02-06 11:14:09 +0200

    Complete L1 T2

---
+++ terraform-engineer/Level-1/Task02/main.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "xfusion-sg" {
+  name        = "xfusion-sg"
+  description = "Security group for Nautilus App Servers"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
